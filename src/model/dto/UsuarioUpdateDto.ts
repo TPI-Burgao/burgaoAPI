8674b6f7071f3b50455c4fb40419fc40852d1699
MizunoYudi <joaomizunovy@gmail.com>
2025-12-06 16:20:40 -0300

    UsuarioUpdateDto criado

---
+++ src/model/dto/UsuarioUpdateDto.ts
@@ -0,0 +1,13 @@
+export class UsuarioUpdateDto {
+    nome: string;
+    email: string;
+    telefone: string;
+
+    constructor(nome: string,
+                email: string,
+                telefone: string){
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+    }
+}
