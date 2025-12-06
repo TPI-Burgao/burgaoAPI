ba4355b4aa0690926f79fb0bc84978bbc512365c
MizunoYudi <joaomizunovy@gmail.com>
2025-12-06 15:58:07 -0300

    UsuarioInsertDto criado

---
+++ src/model/dto/UsuarioInsertDto.ts
@@ -0,0 +1,22 @@
+export class Usuario{
+    cpf: string; 
+    nome: string;
+    email: string;
+    senha: string;
+    telefone: string;
+    dataNascimento: Date;
+
+    constructor(cpf: string,
+                nome: string,
+                email: string,
+                senha: string,
+                telefone: string,
+                dataNascimento: Date){
+        this.cpf = cpf;
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+        this.telefone = telefone;
+        this.dataNascimento = dataNascimento;
+    }
+}
