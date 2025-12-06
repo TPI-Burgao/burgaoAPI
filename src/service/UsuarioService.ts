1b12e19d2a37dc1a0fd03822ca47031925f61d31
MizunoYudi <joaomizunovy@gmail.com>
2025-12-06 18:59:40 -0300

    UsuarioService criando

---
+++ src/service/UsuarioService.ts
@@ -0,0 +1,49 @@
+import { UsuarioRepository } from '../repository/UsuarioRepository';
+import { Usuario } from '../model/entity/Usuario';
+import { UsuarioInsertDto } from '../model/dto/UsuarioInsertDto';
+
+export class UsuarioService {
+    private usuarioRepository = UsuarioRepository.getInstance();
+
+    criarUsuario(data: UsuarioInsertDto) {
+        if(!data.cpf || !data.nome || !data.email || !data.senha || !data.telefone || !data.dataNascimento){
+            throw new Error('Faltam informações para criar o usuário.');
+        }
+        if(this.validarEmail(data.email)){
+            const usuario =  new Usuario(
+                data.cpf,
+                data.nome,
+                data.email,
+                data.senha,
+                data.telefone,
+                data.dataNascimento
+            );
+            return this.usuarioRepository.InsertUsuario(usuario);
+        };
+    }
+
+    private async existeAluno(cpf: string): Promise<boolean> {
+        const usuario = await this.usuarioRepository.BuscarUsuarioPorCPF(cpf);
+        if(usuario == undefined){
+            throw new Error('Usuário não encontrado.');
+        }
+        return true;
+    }       
+
+    private validarEmail(email: string): boolean {
+        let e = email.trim();
+        if (
+            !e.includes(" ") &&
+            e.includes("@") &&
+            e.includes(".") &&
+            e.indexOf("@") != 0 &&
+            e.indexOf("@") < e.indexOf(".") &&
+            e.indexOf(".") > e.indexOf("@") + 1 &&
+            e.indexOf(".") < e.length - 1
+        ) {
+            return true
+        } else {
+            throw new Error("Email invalido");
+        }
+    }
+}
