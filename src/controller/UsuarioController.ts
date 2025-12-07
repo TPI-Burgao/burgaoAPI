9783e6f98ce98b2b495146f28323be325079ccbe
MizunoYudi <joaomizunovy@gmail.com>
2025-12-06 21:51:57 -0300

    UsuarioController: cadastrarUsuario

--- src/controller/UsuarioController.ts
+++ src/controller/UsuarioController.ts
@@ -1,9 +1,26 @@
-import { Route, Tags} from "tsoa";
+import { Body, Post, Res, Route, Tags, TsoaResponse} from "tsoa";
 import { UsuarioService } from "../service/UsuarioService";
+import { UsuarioInsertDto } from "../model/dto/UsuarioInsertDto";
+import { Usuario } from "../model/entity/Usuario";
 
 @Route("usuarios")
 @Tags("Usarios")
 
 export class UsuarioController {
     private usuarioService = new UsuarioService();
+
+    @Post()
+    async cadastrarUsuario(
+        @Body() dto: UsuarioInsertDto,
+        @Res() success: TsoaResponse<201, Usuario | undefined>,
+        @Res() fail: TsoaResponse<400, { message: string }>
+    )
+    {
+        try {
+            const usuario = await this.usuarioService.criarUsuario(dto);
+            return success(201, usuario);
+        } catch (error: any) {
+            return fail(400, { message: `Erro ao cadastrar o Usuário${error.message}` });
+        }
+    }
 }
