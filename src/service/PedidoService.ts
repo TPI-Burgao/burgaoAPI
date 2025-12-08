d1edba5c311713ef9317c2ce6f9d56db596ab058
MizunoYudi <joaomizunovy@gmail.com>
2025-12-08 10:36:46 -0300

    PedidoService: criarPedido

--- src/service/PedidoService.ts
+++ src/service/PedidoService.ts
@@ -4,4 +4,12 @@ import { PedidoRepository } from "../repository/PedidoRepository";
 
 export class PedidoService {
     private pedidoRepository = PedidoRepository.getInstance();
+
+    async criarPedido(data: PedidoDto) {
+        if(!data.usuario){
+            throw new Error('Faltam informações para criar o pedido.');
+        }
+        const pedido =  new Pedido(data.usuario);
+        return this.pedidoRepository.InsertPedido(pedido);
+    }
 }
